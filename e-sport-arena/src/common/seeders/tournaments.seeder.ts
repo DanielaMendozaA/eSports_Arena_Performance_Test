ba4163c454be7f2dc2d63e2860b0508b2c7d41fc
DanielaMendozaA <97774155+DanielaMendozaA@users.noreply.github.com>
2024-10-07 20:59:46 -0500

    feat:competitions service and controller are added

--- e-sport-arena/src/common/seeders/tournaments.seeder.ts
+++ e-sport-arena/src/common/seeders/tournaments.seeder.ts
@@ -6,45 +6,48 @@ import { Tournament } from 'src/tournaments/entities/tournament.entity';
 
 @Injectable()
 export class TournamentsSeeder implements Seeder {
-  constructor(
-    @InjectRepository(Tournament)
-    private readonly tournamentRepository: Repository<Tournament>,
-  ) {}
+    constructor(
+        @InjectRepository(Tournament)
+        private readonly tournamentRepository: Repository<Tournament>,
+    ) { }
 
-  async run(): Promise<void> {
-    const tournaments = [
-      { name: 'Tournament1', startDate: '2024-10-10', endDate: '2024-10-15' },
-      { name: 'Tournament2', startDate: '2024-10-16', endDate: '2024-10-20' },
-      { name: 'Tournament3', startDate: '2024-10-21', endDate: '2024-10-25' },
-      { name: 'Tournament4', startDate: '2024-10-26', endDate: '2024-10-30' },
-      { name: 'Tournament5', startDate: '2024-11-01', endDate: '2024-11-05' },
-      { name: 'Tournament6', startDate: '2024-11-06', endDate: '2024-11-10' },
-      { name: 'Tournament7', startDate: '2024-11-11', endDate: '2024-11-15' },
-      { name: 'Tournament8', startDate: '2024-11-16', endDate: '2024-11-20' },
-      { name: 'Tournament9', startDate: '2024-11-21', endDate: '2024-11-25' },
-      { name: 'Tournament10', startDate: '2024-11-26', endDate: '2024-11-30' },
-      { name: 'Tournament11', startDate: '2024-12-01', endDate: '2024-12-05' },
-      { name: 'Tournament12', startDate: '2024-12-06', endDate: '2024-12-10' },
-      { name: 'Tournament13', startDate: '2024-12-11', endDate: '2024-12-15' },
-      { name: 'Tournament14', startDate: '2024-12-16', endDate: '2024-12-20' },
-      { name: 'Tournament15', startDate: '2024-12-21', endDate: '2024-12-25' },
-    ];
+    async run(): Promise<void> {
+        const tournaments = [
+            { name: 'Tournament22', startDate: '2024-10-10T09:00:00', endDate: '2024-10-15T18:00:00' },
+            { name: 'Tournament30', startDate: '2024-10-16T10:00:00', endDate: '2024-10-20T17:00:00' },
+            { name: 'Tournament50', startDate: '2024-10-21T08:30:00', endDate: '2024-10-25T19:30:00' },
+            { name: 'Tournament60', startDate: '2024-10-26T11:00:00', endDate: '2024-10-30T16:30:00' },
+            { name: 'Tournament70', startDate: '2024-11-01T07:45:00', endDate: '2024-11-05T15:00:00' },
+            { name: 'Tournament60', startDate: '2024-11-06T09:00:00', endDate: '2024-11-10T18:00:00' },
+            { name: 'Tournament70', startDate: '2024-11-11T10:00:00', endDate: '2024-11-15T17:30:00' },
+            { name: 'Tournament80', startDate: '2024-11-16T09:30:00', endDate: '2024-11-20T16:00:00' },
+            { name: 'Tournament90', startDate: '2024-11-21T08:00:00', endDate: '2024-11-25T17:00:00' },
+            { name: 'Tournament100', startDate: '2024-11-26T09:15:00', endDate: '2024-11-30T16:45:00' },
+            { name: 'Tournament110', startDate: '2024-12-01T07:00:00', endDate: '2024-12-05T14:00:00' },
+            { name: 'Tournament120', startDate: '2024-12-06T10:00:00', endDate: '2024-12-10T19:00:00' },
+            { name: 'Tournament130', startDate: '2024-12-11T09:00:00', endDate: '2024-12-15T18:00:00' },
+            { name: 'Tournament140', startDate: '2024-12-16T08:00:00', endDate: '2024-12-20T17:00:00' },
+            { name: 'Tournament150', startDate: '2024-12-21T07:30:00', endDate: '2024-12-25T15:30:00' },
+          ];
 
-    for (const tournamentData of tournaments) {
-      const tournamentExists = await this.tournamentRepository.findOneBy({
-        name: tournamentData.name,
-      });
+        for (const tournamentData of tournaments) {
+            const tournamentExists = await this.tournamentRepository.findOne({
+                where: {
+                    name: tournamentData.name,
+                },
+                withDeleted: true
+            });
 
-      if (!tournamentExists) {
-        const newTournament = this.tournamentRepository.create({
-          ...tournamentData,
-        });
+            if (!tournamentExists) {
+                const newTournament = this.tournamentRepository.create({
+                    ...tournamentData,
+                });
 
-        await this.tournamentRepository.save(newTournament);
-        Logger.log(`Tournament ${tournamentData.name} created`);
-      } else {
-        Logger.log(`Tournament ${tournamentData.name} already exists`);
-      }
+                await this.tournamentRepository.save(newTournament);
+                Logger.log(`Tournament ${tournamentData.name} created`);
+            } else {
+                Logger.log(`Tournament ${tournamentData.name} already exists`);
+            }
+        }
     }
-  }
 }
--- e-sport-arena/src/competitions/competitions.module.ts
+++ e-sport-arena/src/competitions/competitions.module.ts
@@ -3,10 +3,14 @@ import { CompetitionsService } from './competitions.service';
 import { CompetitionsController } from './competitions.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Competition } from './entities/competition.entity';
+import { TournamentsModule } from 'src/tournaments/tournaments.module';
+import { PlayersModule } from 'src/players/players.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Competition])
+    TypeOrmModule.forFeature([Competition]),
+    TournamentsModule,
+    PlayersModule
   ],
   controllers: [CompetitionsController],
   providers: [CompetitionsService],
